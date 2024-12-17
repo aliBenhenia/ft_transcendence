3d538e3d46557af29658a3922c420d28aeba42f3
ali <alibenhenia1@gmail.com>
2024-12-17 10:42:17 +0100

    refactor:style progress level

--- Frontend/src/app/(profile)/profile/profileCard.tsx
+++ Frontend/src/app/(profile)/profile/profileCard.tsx
@@ -17,7 +17,7 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ avatar, first_name, last_name
       className="relative mx-auto bg-[#00152993] rounded-2xl shadow-lg overflow-hidden"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
-      whileHover={{ scale: 1.05, boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.1)' }}
+      whileHover={{boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.1)' }}
       transition={{ duration: 0.3 }}
     >
       <img
@@ -38,7 +38,7 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ avatar, first_name, last_name
           </div>
         </div>
 
-        <header className="mt-6">
+        {/* <header className="mt-6">
           <h3 className="text-white text-sm font-semibold text-center sm:text-left">Level Progress</h3>
           <div className="relative bg-[#0d0e0f] rounded-full h-6 w-full overflow-hidden mt-2">
             <motion.div
@@ -66,7 +66,40 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ avatar, first_name, last_name
           >
             {level}% Completed
           </motion.p>
-        </header>
+        </header> */}
+        
+        <div className="w-full mt-6 sm:mt-0 text-center">
+          <h3 className="text-white text-sm font-semibold  ">Level Progress</h3>
+          <div className="relative bg-[#0d0e0f] rounded-full h-6 w-full overflow-hidden mt-2">
+            <motion.div
+              className="h-full bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 rounded-full shadow-md"
+              style={{ width: `${level}%` }}
+              initial={{ width: '0%' }}
+              animate={{ width: `${level}%` }}
+              transition={{ duration: 1.5, ease: 'easeInOut' }}
+            />
+            <motion.div
+              className="absolute top-1/2 left-1/2 text-sm font-bold text-white transform -translate-x-1/2 -translate-y-1/2 z-[55]"
+              style={{ opacity: level > 0 ? 1 : 0 }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1.5, ease: 'easeInOut' }}
+            >
+              {level}xp
+            </motion.div>
+          </div>
+
+          <motion.p
+            className="text-gray-400 text-sm mt-2 "
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5, duration: 0.5 }}
+          >
+            {level} XP
+          </motion.p>
+        </div>
+
+
       </div>
     </motion.div>
   );
