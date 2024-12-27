2222164c44cf4444e47f2ce6610d35926d0833fd
ali <alibenhenia1@gmail.com>
2024-12-27 13:47:09 +0100

    feat:add accept game

---
+++ Frontend/src/services/accept_game_invite.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+async function acceptGameInvite(roomName) {
+    const token = localStorage.getItem('auth_token'); // Assuming the token is stored in localStorage
+    if (!token) {
+        console.error('No auth token found');
+        return;
+    }
+
+    try {
+        const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/accept_game_invite/`, 
+            { room_name: roomName },
+            {
+                headers: {
+                    'Authorization': `Bearer ${token}`, // Attach token in Authorization header
+                    'Content-Type': 'application/json',
+                }
+            }
+        );
+
+        console.log('Game invite accepted:', response.data);
+        // Handle successful acceptance, such as showing a message or redirecting
+
+    } catch (error) {
+        if (error.response) {
+            console.error('Error accepting invite:', error.response.data.error);
+        } else {
+            console.error('Network error:', error.message);
+        }
+        // Handle error
+    }
+}
+
+
+export default acceptGameInvite;
