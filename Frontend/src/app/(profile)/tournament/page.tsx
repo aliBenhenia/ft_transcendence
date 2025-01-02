23255b50a1a3afb213c1f90fa1677688ed710265
fredrukundo <dukefred9@gmail.com>
2025-01-02 15:16:43 +0100

    tournament page

---
+++ Frontend/src/app/(profile)/tournament/page.tsx
@@ -0,0 +1,9 @@
+function tournament() {
+    return (
+        <div>
+            <h1 className="text-xl text-white">welcome to Tournament</h1>
+        </div>
+    );
+}
+
+export default tournament;
