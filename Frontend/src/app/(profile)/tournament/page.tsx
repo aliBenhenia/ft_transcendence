cb742d41c254ab1356cdcd8accd7c51e71d74870
fredrukundo <dukefred9@gmail.com>
2025-01-03 18:31:41 +0100

    tournament changes

--- Frontend/src/app/(profile)/tournament/page.tsx
+++ Frontend/src/app/(profile)/tournament/page.tsx
@@ -1,7 +1,18 @@
+'use client'
+import React, {useState} from 'react';
+import RegistrationForm from '../components/game/TournamentForm';
+
 function tournament() {
+    const [players, setPlayers] = useState<string[]>(['', '', '', '']);
+    const [tournamentStarted, setTournamentStarted] = useState(false);
+
+    const handleRegister = (playersAlaias: string[]) => {
+        setPlayers(playersAlaias);
+        setTournamentStarted(true);
+    };
     return (
-        <div>
-            <h1 className="text-xl text-white">welcome to Tournament</h1>
+        <div className='max-w-4xl p-8 mx-auto space-y-8'>
+            <RegistrationForm/>
         </div>
     );
 }
