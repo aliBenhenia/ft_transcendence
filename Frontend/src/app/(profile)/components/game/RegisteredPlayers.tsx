d0546df8f96979fa34bb06fe1d6f4b66cb9b1815
fredrukundo <dukefred9@gmail.com>
2025-01-05 07:57:42 +0100

    registered players

--- Frontend/src/app/(profile)/components/game/RegisteredPlayers.tsx
+++ Frontend/src/app/(profile)/components/game/RegisteredPlayers.tsx
@@ -1,27 +1,3 @@
-// 'use client';
-
-// import React from 'react';
-
-// interface RegisteredPlayersProps {
-//   players: { alias: string; avatar: string }[];
-// }
-
-// const RegisteredPlayers: React.FC<RegisteredPlayersProps> = ({ players }) => (
-//   <div>
-//     <h2 className="text-xl text-white font-bold items-center justify-center flex py-4">Registered Players</h2>
-//     <ul className="space-4 flex items-center justify-center">
-//       {players.map((player, index) => (
-//         <li key={index} className="flex items-center space-x-4 p-4 bg-gray-500 rounded-2xl shadow-md">
-//           <img src={player.avatar} alt="avatar" className="w-10 h-10 rounded-full" />
-//           <span className="font-bold text-white text-lg">{player.alias}</span>
-//         </li>
-//       ))}
-//     </ul>
-//   </div>
-// );
-
-// export default RegisteredPlayers;
-
 'use client';
 
 import React from 'react';
