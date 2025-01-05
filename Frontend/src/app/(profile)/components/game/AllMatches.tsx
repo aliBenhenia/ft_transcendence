03267048042e203834fc09d83e32a980837deda7
fredrukundo <dukefred9@gmail.com>
2025-01-05 07:59:33 +0100

    All matches to be played

--- Frontend/src/app/(profile)/components/game/AllMatches.tsx
+++ Frontend/src/app/(profile)/components/game/AllMatches.tsx
@@ -1,35 +1,3 @@
-// 'use client';
-
-// import React from 'react';
-
-// interface Match {
-//   player1: { alias: string; avatar: string };
-//   player2: { alias: string; avatar: string };
-// }
-
-// interface AllMatchesProps {
-//   matches: Match[];
-// }
-
-// const AllMatches: React.FC<AllMatchesProps> = ({ matches }) => (
-//   <div>
-//     <h2 className="mt-4 text-lg font-bold text-white pb-4">All Matches</h2>
-//     <ul className="space-y-2">
-//       {matches.map((match, index) => (
-//         <li key={index} className="flex items-center space-x-4">
-//           <img src={match.player1.avatar} alt="avatar" className="w-8 h-8 rounded-full" />
-//           <span className="text-sm font-medium text-white">{match.player1.alias}</span>
-//           <span className="text-sm font-medium text-red-400">vs</span>
-//           <img src={match.player2.avatar} alt="avatar" className="w-8 h-8 rounded-full" />
-//           <span className="text-sm font-medium text-white">{match.player2.alias}</span>
-//         </li>
-//       ))}
-//     </ul>
-//   </div>
-// );
-
-// export default AllMatches;
-
 'use client';
 
 import React from 'react';
@@ -46,7 +14,7 @@ interface AllMatchesProps {
 const AllMatches: React.FC<AllMatchesProps> = ({ matches }) => (
   <div className="max-w-3xl mx-auto p-6 bg-[#06294a] rounded-lg shadow-2xl mt-5">
     <h2 className="text-2xl text-gray-100 font-extrabold text-center mb-6">
-      All Matches
+      All Matches to be played
     </h2>
     <ul className="space-y-4">
       {matches.map((match, index) => (
