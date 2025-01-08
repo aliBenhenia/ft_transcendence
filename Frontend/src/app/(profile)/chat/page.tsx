22212ce85dc605f65fe08ce4f37879a1c94b843b
ali <alibenhenia1@gmail.com>
2025-01-08 10:30:25 +0100

    style :break words, add msg length up to 4000 characters

--- Frontend/src/app/(profile)/chat/page.tsx
+++ Frontend/src/app/(profile)/chat/page.tsx
@@ -138,9 +138,9 @@ export default function ChatPage() {
 
   const sendMessage = async () => {
     if (!newMessage || !selectedUser?.on_talk || isSending || !token) return
-    if (newMessage.length > 700)
+    if (newMessage.length > 4100)
     {
-      message.error('message must be less then 700');
+      message.error('message must be less then 4100');
       return;
     }
     const messagePayload = {
@@ -315,7 +315,7 @@ export default function ChatPage() {
         </aside>
 
         {/* Chat Area */}
-        <main className="flex-1 flex flex-col bg-gray-900 overflow-hidden">
+        <main className="flex-1 flex flex-col bg-gray-900 overflow-auto">
           {selectedUser ? (
             <>
               {/* Chat Header */}
@@ -342,7 +342,7 @@ export default function ChatPage() {
               </div>
 
               {/* Messages */}
-              <div className="flex-1 overflow-y-auto p-4 space-y-4 mb-[140px]">
+              <div className="flex-1 overflow-y-auto p-4 space-y-4 mb-[140px] overflow-x-auto">
                 {loading ? (
                   <div className="flex justify-center items-center h-full">
                     <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
@@ -353,7 +353,7 @@ export default function ChatPage() {
                   messages.map((msg:any, index:number) => (
                     <div
                       key={index}
-                      className={`flex ${msg.sender !== selectedUser.on_talk ? 'justify-end' : 'justify-start'}`}
+                      className={`flex break-all ${msg.sender !== selectedUser.on_talk ? 'justify-end' : 'justify-start'}`}
                     >
                       <div
                         className={`max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl px-4 py-2 rounded-lg ${
