740cf895c5387afc35a819c824abcbf37a2fab35
fredrukundo <dukefred9@gmail.com>
2025-01-19 11:16:46 +0100

    fix responsivee

--- Frontend/src/app/(profile)/game/AiOpponent/page.tsx
+++ Frontend/src/app/(profile)/game/AiOpponent/page.tsx
@@ -5,7 +5,7 @@ import Ai from "../../components/game/OfflineGame";
 const AiOpponent = () => {
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-screen">
+    <div className="flex flex-col items-center justify-center h-screen">
         <Ai/>
     </div>
   );
